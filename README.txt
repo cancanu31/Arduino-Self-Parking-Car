fbee62d1c5f658d0fbb4564c020593c045dbd7a4
Catalin-Adrian Sas <catalinsas99@gmail.com>
2020-12-17 19:43:08 +0200

    first

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
--mi am construit masinuta si m am uitat peste cum sa leg firele
--am inceput implementarea codului
--urmeaza sa leg firele in zilele urmatoare(dupa ce imi fac rost de pistol de lipit)
+-am reusit sa fac masina sa mearga si sa conectez firele
+-urmeaza ca maine sa mi ajunga si setul de fire mama tata ca sa pot conecta senzorul 
+ultrasonic dupa care incep sa scriu restul de cod 
+
--- parcare_laterala.ino
+++ parcare_laterala.ino
@@ -1,85 +1,31 @@
-// Pinii motor 1
-#define mpin00 5
-#define mpin01 6
-// Pinii motor 2
-#define mpin10 3
-#define mpin11 11
-
-#define left 0 //left direction command
-#define right 1 //right direction command
-#define forward 2 //forward command 
-#define reverse 3 //reverse command 
-#define car_widht 15 //width of the car(cm)
-#define car_length 20 //car length(cm)
-
-//trebe definiti senzorii ultrasonici
-
-void motor_setup() {
-// configurarea pinilor motor ca iesire, initial valoare 0
-digitalWrite(mpin00, 0);
-digitalWrite(mpin01, 0);
-digitalWrite(mpin10, 0);
-digitalWrite(mpin11, 0);
-pinMode (mpin00, OUTPUT);
-pinMode (mpin01, OUTPUT);
-pinMode (mpin10, OUTPUT);
-pinMode (mpin11, OUTPUT);
-
-}
-
-// Funcție pentru controlul unui motor
-// Intrare: pinii m1 și m2, direcția și viteza
-void StartMotor (int m1, int m2, int forward, int speed)
-{
-if (speed==0) // oprire
-{
-digitalWrite(m1, 0);
-digitalWrite(m2, 0);
-}
-else
-{
-if (forward)
-{
-digitalWrite(m2, 0);
-analogWrite(m1, speed); // folosire PWM
-}
-else
-{
-digitalWrite(m1, 0);
-analogWrite(m2, speed);
-}
-}
-}
-
-// Funcție de siguranță
-// Execută oprire motoare, urmată de delay
-void delayStopped(int ms)
-{
-StartMotor (mpin00, mpin01, 0, 0);
-StartMotor (mpin10, mpin11, 0, 0);
-delay(ms);
-}
-
-bool Park_Location_Check()
-{
- //verifica daca are loc sa faca parcarea
- //daca e totul ok returneaza o valoare booleana 
- //urmeaza a fi implementat
-}
-
-void Car_Park()
-{
-  //se va apela la inceput Park_Location_Check()
-  //daca este totul in ordine se face parcarea propriu zisa
-  //urmeaza a fi implementat
-
-}
+#define enA 9
+#define enB 6
+#define in11 5
+#define in12 4
+#define in21 3
+#define in22 2
+#define car_widht 15 //latimea masini(cm)
+#define car_length 20 //lungimea masinii(cm)
 
 void setup()
 {
-  
+  pinMode(enA,OUTPUT);
+  pinMode(enB,OUTPUT);
+  pinMode(in11,OUTPUT);//de la motoare i11..i22
+  pinMode(in12,OUTPUT);
+  pinMode(in21,OUTPUT);
+  pinMode(in22,OUTPUT);
+
+  digitalWrite(in11,LOW);
+  digitalWrite(in12,HIGH);
+  digitalWrite(in21,LOW);
+  digitalWrite(in22,HIGH);
+  Serial.begin(9600);//lib de in out
 }
 
-void loop() {
-Car_Park();
+void loop()
+{
+  analogWrite(enA,255);
+  analogWrite(enB,255);
+  Serial.print("MASINA MERGE");
 }
